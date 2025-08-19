4cb17122f97ef2b94b0bf1715f896e7f465b5f83
joao paulo <joaopaulopintoqueiros4pgmail.com>
2025-08-20 01:55:12 +0200

    amélioration globale

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 // Attendre que la page soit entièrement chargée
+
 document.addEventListener('DOMContentLoaded', function() {
 
 
-    // Ici tout notre code JavaScript
+    //code JavaScript
+
     console.log("JavaScript externe chargé !");
 
 
     // Trouve l'élément et change son texte
 
-    const youtubeElement = document.getElementById('youtube-count');
-    youtubeElement.textContent = '1,2K';
-    
+    fetch('https://www.youtube.com/@Joao-Paulo-Pinto-Queiros')
+        .then(response => response.json())
+        .then(data => {
+            const youtubeElement = document.getElementById('youtube-count');
+            if (data.items && data.items.length > 0) {
+                youtubeElement.textContent = data.items[0].statistics.subscriberCount;
+            } else {
+                youtubeElement.textContent = "Non trouvé";
+            }
+            console.log("Données YouTube :", data);
+        })
+
+        .catch(error => {
+            console.log("Erreur YouTube :", error);
+            const youtubeElement = document.getElementById('youtube-count');
+            youtubeElement.textContent = "Erreur API";
+        });
+
     const twitchElement = document.getElementById('twitch-count');
     twitchElement.textContent = '856';
     
